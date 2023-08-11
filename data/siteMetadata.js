77b73b643d637fc37f50f1834d8cf7a107704e93
giuszeppe <giuseppe@mwspace.com>
2023-08-12 00:09:21 +0200

    changed metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,7 +17,7 @@ const siteMetadata = {
   twitterHandle: '@BraydonCoyer',
   codepen: 'https://codepen.io/braydoncoyer',
   linkedin: 'https://www.linkedin.com/in/braydon-coyer/',
-  resume: '/static/braydoncoyer_resume.pdf',
+  resume: '/static/giuseppe_cv.pdf',
   snippets:
     'https://www.notion.so/9f35162787e0448094188b66304cf9f6?v=1b9c4e146edf46148fe7b1c5346cbea4',
   carbonCode: 'CESDE27N',
